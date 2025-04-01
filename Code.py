d949dac6bd21c6c3a5c0f75f4d47898ed7a8c2d7
tomiaracodes <maraba1@jh.edu>
2025-04-01 20:11:20 +0000

    Setting up

---
+++ Code.py
@@ -0,0 +1,37 @@
+# class for reading bff file
+# another class to udnerstand blocks
+# another class for laser trajectory
+# attribute number to x   
+class grid(unsolved_file, solved_file)
+
+    def __init__(self):
+        
+    def read_bff(unsolved_file):
+
+class blocks:
+    
+    def __init__(self, A, B, C):
+    
+        self.A = 0
+        self.B = 0
+        self.C = 0
+    '''Initialise number of reflect, 
+    opaque and refract blocks to be 0'''
+
+    def block_type(self, A,B,C):
+        countA = 0
+        countB = 0
+        countC = 0
+        if A:
+            countA = countA+1
+        if B:
+            countB = countB+1
+        if C:
+            countC = countC+1
+
+class refract(blocks):
+     
+class laser_move:
+    
+
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Lazor_Project
 Software Carpentry Spring 2025 Lazor Project
+
+# We can think of the grid as having a specified number of blocks; opaque, reflect or refract blocks
+# With opaque blocks, the laser will reflect off of it - at which angle?
+# With opaque blocks, the laser will 
